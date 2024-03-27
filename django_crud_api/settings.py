5457dfb065054278d97bdf4e038c4b28bdedc32d
David <davidbriangarcia@gmail.com>
2024-03-26 23:00:51 -0500

    including docs

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'corsheaders',
+    'coreapi',
     'rest_framework'
 ]
 
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = []
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from rest_framework import routers
 from tasks import views
 
 router = routers.DefaultRouter()
 router.register(r'products', views.ProductView, 'Products')
 urlpatterns = [
-    path("api/v1/", include(router.urls))
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title="API"))
 ]
