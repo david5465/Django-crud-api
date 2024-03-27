8e29be5fc752d87220b29e4e4c12c95e38d255af
David <davidbriangarcia@gmail.com>
2024-03-26 21:39:47 -0500

    crud sin frontend

---
+++ tasks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework import routers
+from tasks import views
+
+router = routers.DefaultRouter()
+router.register(r'products', views.ProductView, 'Products')
+urlpatterns = [
+    path("api/v1/", include(router.urls))
+]
