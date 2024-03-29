1ed5b0b69f8148113b38514aa9b3fe0203e2a296
David <davidbriangarcia@gmail.com>
2024-03-29 14:15:05 -0500

    update urls.py

--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('tasks.urls'))
+    path('products/', include('tasks.urls')),
+    path('', RedirectView.as_view(url='/products/api/v1/products', permanent=True))
 ]
